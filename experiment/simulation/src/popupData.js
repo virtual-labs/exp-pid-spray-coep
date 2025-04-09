ffd2b5e731489108d0b6cbdd4c6e81aa991157bd
Vaibhav Kurhe <vrk.vlab@coeptech.ac.in>
2025-04-09 10:06:18 +0530

    updated

--- experiment/simulation/src/popupData.js
+++ experiment/simulation/src/popupData.js
@@ -4,14 +4,14 @@ $("#simDemo").click(function () {
 		
 		+'<div class="row statement" ><p>A problem statement is a crucial element in designing and developing a pilot plant. '
 		+'It defines the problem or opportunity that the pilot plant aims to address, and it serves as a guiding force throughout the project.'
-		+'Heres a general outline to help you define a problem statement for designing and developing a pilot plant:'
-		+'Problem Statement: You are given the responsibility as an Instrumentation and Control Engineer to design and commission a pilot '
-		+'<br><b>Spray dryer of 5 Liter capacity.</b>'
+		+' Here is a general outline to help you define a problem statement for designing and developing a pilot plant.'
+		+'<br><br><b>Problem Statement:</b> You are given the responsibility as an Instrumentation and Control Engineer to design and commission a pilot plant '
+		+'of Spray dryer of 5 Liter capacity.'
 		+'<br><p><b>Background :</b> <br>The pilot plant will be used to test the new “control and automation” technologies.'
-		+'<br><b>Key Performance Indicators (KPIs) :</b><br> You will be judged based on the accuracy of design, proper'
-		+'selection of field and panel instruments, and successful commissioning of the plant in a stipulated time frame.'
-		+'<br><b>Constraints and Assumptions :</b><br> As you are an Instrumentation and Control Engineer process related details are'
-		+'not expected from you. You will receive the same from a process expert.</b></div>'
+		+'<br><br><b>Key Performance Indicators (KPIs) :</b><br> You will be judged based on the accuracy of design, proper'
+		+' selection of field and panel instruments, and successful commissioning of the plant in a stipulated time frame.'
+		+'<br><br><b>Constraints and Assumptions :</b><br> As you are an Instrumentation and Control Engineer process related details are'
+		+' not expected from you. You will receive the same from a process expert.</b></div>'
 		+'<div class="row"><img src="images/realplant.png" class="img img-responsive"></img></div>'
 
 		
